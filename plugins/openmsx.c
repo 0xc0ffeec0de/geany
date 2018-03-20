d14afa1d237998ec498d5621d7ba69adfccbd975
Pedro de Medeiros <pedro.medeiros@gmail.com>
2018-03-20 09:54:38 -0300

    First commit.

--- plugins/openmsx.c
+++ plugins/openmsx.c
@@ -6,6 +6,10 @@
  */
 
 
+#ifdef HAVE_CONFIG_H
+#	include "config.h"
+#endif
+
 #include "geanyplugin.h"	/* plugin API, always comes first */
 
 
@@ -38,6 +42,7 @@ enum State
 
 static struct
 {
+	GtkWidget *main;
 	GtkWidget *start;
 	GtkWidget *pause;
 	GtkWidget *stop;
@@ -53,13 +58,13 @@ static GtkWidget *main_menu_item = NULL;
 
 typedef struct OpenMSXWindow
 {
-	GtkWidget		*socket;		/* OpenMSX plugin window */
+	GtkSocket		*socket;		/* OpenMSX plugin window */
 	GtkWidget		*vbox;
 	GtkWidget		*name_label;
 }
 OpenMSXWindow;
 
-static EditWindow openmsx_window = {NULL, NULL, NULL, NULL};
+static OpenMSXWindow openmsx_window = {NULL, NULL, NULL};
 
 
 typedef struct OpenMSXDialog
@@ -76,37 +81,82 @@ typedef struct OpenMSXUserData
 {
 	gchar *command;
 	gint socket;
-	gchar *parameters;
 	gchar **saved_states;
 	int num_saved_states;
+	GPid pid;
 }
 OpenMSXUserData;
 
-const gchar *DEFAULT_COMMAND = "/usr/bin/openmsx";
+static const gchar *DEFAULT_COMMAND = "/usr/bin/openmsx";
 
-const gchar *DEFAULT_PARAMETERS = "";
-
-static OpenMSXUserData openmsx_userdata = {
-	DEFAULT_COMMAND,
-	0,
-	DEFAULT_PARAMETERS,
+static OpenMSXUserData openmsx_userdata =
+{
 	NULL,
-	0
+	0,
+	NULL,
+	0,
+	0,
 };
 
+static void split_view();
+
+static void unsplit_view();
+
+static void on_openmsx_activate(GObject *obj, GeanyDocument *doc, gpointer user_data)
+{
+}
+
+static void on_openmsx_close(GObject *obj, GeanyDocument *doc, gpointer user_data)
+{
+}
 
 static PluginCallback openmsx_callbacks[] =
 {
 	{ "document-activate", (GCallback) &on_openmsx_activate, TRUE, NULL },
-	{ "document-close", (GCallback) &on_opemsx_close, TRUE, NULL },
-	{ "project-before-close", (GCallback) &on_opemsx_close, TRUE, NULL },
+	{ "project-before-close", (GCallback) &on_openmsx_close, TRUE, NULL },
 	{ NULL, NULL, FALSE, NULL }
 };
 
+/* emulator control routines */
 
 static void start_emulator()
 {
+	GError *error;
+	gint argcp;
+	gchar **argvp;
 
+	if (openmsx_userdata.pid == 0
+		&& g_shell_parse_argv(openmsx_userdata.command, &argcp, &argvp, &error))
+	{
+		gchar *envp[2] = { g_strdup_printf("SDL_WINDOWID=%i", gtk_socket_get_id(openmsx_window.socket)), NULL };
+		gboolean b = g_spawn_async(NULL, argvp, envp, G_SPAWN_SEARCH_PATH
+			| G_SPAWN_FILE_AND_ARGV_ZERO, 0, NULL, &openmsx_userdata.pid, &error);
+
+		if (!b)
+		{
+			ui_set_statusbar(FALSE, _("OpenMSX executable not found!"));
+		}
+		else
+		{
+			/* estabilish connection with pipes. */
+		}
+	}
+}
+
+static void resume_emulator()
+{
+
+}
+
+static void pause_emulator()
+{
+
+}
+
+static void stop_emulator()
+{
+	g_spawn_close_pid(openmsx_userdata.pid);
+	openmsx_userdata.pid = 0;
 }
 
 static void on_start_emulator(GtkMenuItem *menuitem, gpointer user_data)
@@ -154,7 +204,7 @@ static void split_view()
 	g_object_ref(notebook);
 	gtk_container_remove(GTK_CONTAINER(parent), notebook);
 
-	pane = horizontal ? gtk_hpaned_new() : gtk_vpaned_new();
+	pane = gtk_hpaned_new();
 	gtk_container_add(GTK_CONTAINER(parent), pane);
 
 	gtk_container_add(GTK_CONTAINER(pane), notebook);
@@ -163,14 +213,14 @@ static void split_view()
 	box = gtk_vbox_new(FALSE, 0);
 	gtk_container_add(GTK_CONTAINER(pane), box);
 
-	toolbar = create_toolbar();
-	gtk_box_pack_start(GTK_BOX(box), toolbar, FALSE, FALSE, 0);
+	//toolbar = create_toolbar();
+	//gtk_box_pack_start(GTK_BOX(box), toolbar, FALSE, FALSE, 0);
 	openmsx_window.vbox = box;
 
 	socket = gtk_socket_new();
 	gtk_widget_set_size_request(socket, 640, 480);
-	gtk_box_pack_start(GTK_BOX(box), toolbar, TRUE, FALSE, 0);
-	openmsx_window.socket = socket;
+	gtk_box_pack_start(GTK_BOX(box), socket, TRUE, TRUE, 0);
+	openmsx_window.socket = GTK_SOCKET(socket);
 
 	gtk_paned_set_position(GTK_PANED(pane), width);
 	gtk_widget_show_all(pane);
@@ -189,14 +239,16 @@ static void unsplit_view()
 	GtkWidget *pane = gtk_widget_get_parent(notebook);
 	GtkWidget *parent = gtk_widget_get_parent(pane);
 
-	set_state(STATE_UNSPLIT);
+	set_state(STATE_STOPPED);
 
 	g_object_ref(notebook);
 	gtk_container_remove(GTK_CONTAINER(pane), notebook);
 
 	gtk_widget_destroy(pane);
-	edit_window.editor = NULL;
-	edit_window.sci = NULL;
+	gtk_widget_destroy(openmsx_window.vbox);
+	gtk_widget_destroy(GTK_WIDGET(openmsx_window.socket));
+	openmsx_window.vbox = NULL;
+	openmsx_window.socket = NULL;
 
 	gtk_container_add(GTK_CONTAINER(parent), notebook);
 	g_object_unref(notebook);
@@ -244,9 +296,6 @@ static gboolean openmsx_init(GeanyPlugin *plugin, gpointer data)
 	//keybindings_set_item(key_group, KB_SPLIT_UNSPLIT, kb_activate,
 		//0, 0, "split_unsplit", _("_Unsplit"), menu_items.unsplit);
 
-	/* keep a pointer to the menu item, so we can remove it when the plugin is unloaded */
-	main_menu_item = openmsx_item;
-
 	return TRUE;
 }
 
@@ -261,8 +310,7 @@ on_configure_response(GtkDialog *dialog, gint response, gpointer user_data)
 
 		OpenMSXDialog *dialog = user_data;
 
-		openmsx_userdata.command = g_strdup(gtk_entry_get_text(GTK_ENTRY(dialog.command_entry)));
-		openmsx_userdata.parameters = g_strdup(gtk_entry_get_text(GTK_ENTRY(dialog.parameters_entry)));
+		openmsx_userdata.command = g_strdup(gtk_entry_get_text(GTK_ENTRY(dialog->command_entry)));
 	}
 }
 
@@ -284,7 +332,9 @@ static GtkWidget *openmsx_configure(GeanyPlugin *plugin, GtkDialog *dialog, gpoi
 	gtk_misc_set_alignment(GTK_MISC(label), 0, 0.5);
 	entry = gtk_entry_new();
 
-	if (openmsx_userdata.command != NULL)
+	if (openmsx_userdata.command == NULL)
+		gtk_entry_set_text(GTK_ENTRY(entry), g_strdup(DEFAULT_COMMAND));
+	else
 		gtk_entry_set_text(GTK_ENTRY(entry), openmsx_userdata.command);
 
 	gtk_container_add(GTK_CONTAINER(vbox), label);
@@ -295,9 +345,6 @@ static GtkWidget *openmsx_configure(GeanyPlugin *plugin, GtkDialog *dialog, gpoi
 	gtk_misc_set_alignment(GTK_MISC(label), 0, 0.5);
 	entry = gtk_entry_new();
 
-	if (openmsx_userdata.parameters != NULL)
-		gtk_entry_set_text(GTK_ENTRY(entry), openmsx_userdata.parameters);
-
 	gtk_container_add(GTK_CONTAINER(vbox), label);
 	gtk_container_add(GTK_CONTAINER(vbox), entry);
 	openmsx_dialog.parameters_entry = entry;
@@ -307,7 +354,7 @@ static GtkWidget *openmsx_configure(GeanyPlugin *plugin, GtkDialog *dialog, gpoi
 	// TODO: save state GUI elements.
 
 	/* Connect a callback for when the user clicks a dialog button */
-	g_signal_connect(dialog, "response", G_CALLBACK(on_configure_response), openmsx_dialog);
+	g_signal_connect(dialog, "response", G_CALLBACK(on_configure_response), &openmsx_dialog);
 
 	return vbox;
 }
@@ -321,11 +368,12 @@ static void openmsx_cleanup(GeanyPlugin *plugin, gpointer data)
 	/* remove the menu item added in openmsx_init() */
 	gtk_widget_destroy(main_menu_item);
 
+	if (openmsx_userdata.pid != 0)
+		stop_emulator();
+
 	/* release other allocated strings and objects */
-	if (openmsx_userdata.command != DEFAULT_COMMAND)
+	if (openmsx_userdata.command != NULL)
 		g_free(openmsx_userdata.command);
-	if (openmsx_userdata.parameters != DEFAULT_PARAMETERS)
-		g_free(openmsx_userdata.parameters);
 }
 
 void geany_load_module(GeanyPlugin *plugin)
